ef063e015c375d5e56f86101afe30042efecb9a5
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-05-07 11:36:53 +0530

    added shared components avatar and card

--- FRONTEND/src/user/components/UserItem.jsx
+++ FRONTEND/src/user/components/UserItem.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React from "react";
+import Avatar from "../../shared/components/UIElements/Avatar";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
 
+import "./UserItem.css";
 
-import './UserItem.css';
-
-const UserItem = ({key, id, image, name, placeCount}) => {
+const UserItem = ({ key, id, image, name, placeCount }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>
-            {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            {/* <img src={image} alt={name} /> */}
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>
+              {placeCount} {placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
