a3075cb62dd7d6f9abb2ece5a3d696ba9c046fa3
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-05-20 12:56:25 +0530

    adding custom form input component

--- FRONTEND/src/places/pages/NewPlace.jsx
+++ FRONTEND/src/places/pages/NewPlace.jsx
@@ -1,5 +1,13 @@
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
+
 const NewPlace = () => {
-    return <p>This is the new NewPlace</p>
-}
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
+};
 
 export default NewPlace;
