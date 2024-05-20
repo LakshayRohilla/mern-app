7bf6ba6bae41604d3d84360bda788fefcccd8b11
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-05-20 15:20:15 +0530

    managing state in the input component using reducers

--- FRONTEND/src/places/pages/NewPlace.jsx
+++ FRONTEND/src/places/pages/NewPlace.jsx
@@ -4,9 +4,16 @@ import './NewPlace.css';
 
 const NewPlace = () => {
   return (
-    <form className="place-form">
-      <Input element="input" type="text" label="Title" />
-    </form>
+    
+      <form className="place-form">
+        <Input
+          element="input"
+          type="text"
+          label="Title"
+          validators={[]}
+          errorText="Please enter a valid title."
+        />
+      </form>
   );
 };
 
