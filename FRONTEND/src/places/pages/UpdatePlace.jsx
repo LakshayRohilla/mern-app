b533d18838378723c69d655a8572a84e9df8cb10
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-05-22 22:04:05 +0530

    Updated update places file to display msg on place not exist and in placelist added clickable button

--- FRONTEND/src/places/pages/UpdatePlace.jsx
+++ FRONTEND/src/places/pages/UpdatePlace.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import Card from "../../shared/components/UIElements/Card";
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH,
@@ -60,19 +61,21 @@ const UpdatePlace = () => {
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true,
+    if (identifiedPlace) { // if we dont add this then when there wont be any place we will get an error.
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true,
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true,
+          },
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true,
-        },
-      },
-      true
-    );
+        true
+      );
+    }
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
@@ -84,7 +87,7 @@ const UpdatePlace = () => {
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place!</h2>
+        <Card><h1>Could not find place!</h1></Card>
       </div>
     );
   }
