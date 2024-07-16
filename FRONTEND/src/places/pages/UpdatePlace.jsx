3e5bb41d602e26446b052e49a9b4b734e93306a2
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-07-16 18:23:27 +0530

    preparing api keys and credentials

--- FRONTEND/src/places/pages/UpdatePlace.jsx
+++ FRONTEND/src/places/pages/UpdatePlace.jsx
@@ -40,7 +40,7 @@ const UpdatePlace = () => {
     const fetchPlace = async () => {
       try {
         const responseData = await sendRequest(
-          `${process.env.}/places/${placeId}`
+          `${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`
         );
         setLoadedPlace(responseData.place);
         setFormData(
